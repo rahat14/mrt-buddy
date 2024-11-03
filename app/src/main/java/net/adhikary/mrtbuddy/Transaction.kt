3a41fd99ed787bcab00fde56e8a506ef9fd8b873
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-03 21:54:17 +0600

    chore: remove duplicate data class

--- app/src/main/java/net/adhikary/mrtbuddy/Transaction.kt
+++
@@ -1,9 +0,0 @@
-data class Transaction(
-    val fixedHeader: String,
-    val timestamp: String,
-    val transactionType: String,
-    val fromStation: Int,
-    val toStation: Int,
-    val balance: Int,
-    val trailing: String
-)
