e1c1e27e096a326e22ae481b9d8a1d41fee855c1
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2024-11-04 02:36:50 +0600

    fix: station names

--- app/src/main/java/net/adhikary/mrtbuddy/MainActivity.kt
+++ app/src/main/java/net/adhikary/mrtbuddy/MainActivity.kt
@@ -306,9 +306,12 @@ class MainActivity : ComponentActivity() {
         // Map station codes to names
         val stationMap = mapOf(
             10 to "Motijheel",
+            20 to "Bangladesh Secretariat",
+            25 to "Dhaka University",
             30 to "Shahbagh",
             35 to "Karwan Bazar",
             40 to "Farmgate",
+            45 to "Bijoy Sarani",
             50 to "Agargaon",
             55 to "Shewrapara",
             60 to "Kazipara",
