3740718611ae1fd901faf04cd043fefad1d43a44
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-06 02:59:27 +0800

    chore: refactor out the nfc command generation logic

--- app/src/main/java/net/adhikary/mrtbuddy/nfc/NfcReader.kt
+++ app/src/main/java/net/adhikary/mrtbuddy/nfc/NfcReader.kt
@@ -14,38 +14,14 @@ class NfcReader {
     private val timestampService = TimestampService()
     private val stationService = StationService()
     private val transactionParser = TransactionParser(byteParser, timestampService, stationService)
+    private val commandGenerator = NfcCommandGenerator()
 
     fun readTransactionHistory(nfcF: NfcF): List<Transaction> {
         val transactions = mutableListOf<Transaction>()
         val idm = nfcF.tag.id
-        val serviceCode = 0x220F
-        val serviceCodeList = byteArrayOf(
-            (serviceCode and 0xFF).toByte(),
-            ((serviceCode shr 8) and 0xFF).toByte()
-        )
-
-        val numberOfBlocksToRead = 10
-
-        val blockListElements = ByteArray(numberOfBlocksToRead * 2)
-        for (i in 0 until numberOfBlocksToRead) {
-            blockListElements[i * 2] = 0x80.toByte()
-            blockListElements[i * 2 + 1] = i.toByte()
-        }
-
-        val commandLength = 14 + blockListElements.size
-        val command = ByteArray(commandLength)
-        var idx = 0
-        command[idx++] = commandLength.toByte()
-        command[idx++] = 0x06.toByte()
-        System.arraycopy(idm, 0, command, idx, idm.size)
-        idx += idm.size
-        command[idx++] = 0x01.toByte()
-        command[idx++] = serviceCodeList[0]
-        command[idx++] = serviceCodeList[1]
-        command[idx++] = numberOfBlocksToRead.toByte()
-        System.arraycopy(blockListElements, 0, command, idx, blockListElements.size)
 
         try {
+            val command = commandGenerator.generateReadCommand(idm)
             val response = nfcF.transceive(command)
             transactions.addAll(transactionParser.parseTransactionResponse(response))
         } catch (e: IOException) {
