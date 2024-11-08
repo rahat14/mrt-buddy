382466ac7e920fe36f5124b298fff50c93c0f34d
Rahatul Aseaikin Shovo <27925309+rahat14@users.noreply.github.com>
2024-11-08 19:32:03 +0600

    added Ios Db

--- composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/database/IosDatabase.kt
+++ composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/database/IosDatabase.kt
@@ -2,7 +2,6 @@ package net.adhikary.mrtbuddy.database
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import platform.Foundation.NSHomeDirectory
 
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
