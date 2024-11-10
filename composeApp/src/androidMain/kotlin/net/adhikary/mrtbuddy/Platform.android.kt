0db11b42aa45324f5f9faa0955eacb0cbf59232a
Rahatul Aseaikin Shovo <27925309+rahat14@users.noreply.github.com>
2024-11-11 00:24:16 +0600

    added a way to find build type

--- composeApp/src/androidMain/kotlin/net/adhikary/mrtbuddy/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/net/adhikary/mrtbuddy/Platform.android.kt
@@ -7,3 +7,5 @@ class AndroidPlatform : Platform {
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual val isDebug: Boolean = BuildConfig.DEBUG
--- composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/Platform.ios.kt
@@ -1,9 +1,13 @@
 package net.adhikary.mrtbuddy
 
 import platform.UIKit.UIDevice
+import kotlin.experimental.ExperimentalNativeApi
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+@OptIn(ExperimentalNativeApi::class)
+actual val isDebug: Boolean = KotlinPlatform.isDebugBinary
