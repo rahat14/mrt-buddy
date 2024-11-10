c7fba6c9f19e90aca2efc1233ac79f61d2c06b36
Rahatul Aseaikin Shovo <27925309+rahat14@users.noreply.github.com>
2024-11-11 00:23:46 +0600

    ios impl added

--- composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/MainViewController.kt
@@ -2,12 +2,19 @@ package net.adhikary.mrtbuddy
 
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import net.adhikary.mrtbuddy.database.getDatabase
 
 fun MainViewController() = ComposeUIViewController {
     val dao = remember {
         getDatabase().getDao()
     }
+    if (isDebug) {
+        Napier.base(DebugAntilog())
+    }
+    
+
     App(dao = dao)
 
 }
