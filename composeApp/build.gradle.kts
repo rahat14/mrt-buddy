3bdf3f8e10d738d568bb5b1f6d2d4494bf9c3d4a
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-11 07:41:59 +0800

    chore: remove dependenciesInfo from apk for f-droid signing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -99,6 +99,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
 }
 
 
