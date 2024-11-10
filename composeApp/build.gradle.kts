37aa9932ac4b05141519f7e2faf6422e0ee8f698
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-10 17:03:42 +0800

    chore: bump version to 0.0.10

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,8 +65,8 @@ android {
         applicationId = "net.adhikary.mrtbuddy"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.0.10"
     }
     packaging {
         resources {
